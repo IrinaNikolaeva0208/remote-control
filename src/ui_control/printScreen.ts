993997709fd068a9b3c216791fac1cab1d99358d
Dark_Soul <irina2002.nikolaeva@yandex.ru>
2023-01-23 23:17:11 +0300

    fix: fix out-of-screen error

--- src/ui_control/printScreen.ts
+++ src/ui_control/printScreen.ts
@@ -1,5 +1,8 @@
 import {screen, Region, mouse,} from "@nut-tree/nut-js"
 import {readFileSync} from "fs"
+import { resolve } from "path";
+
+const errorPath = resolve("src", "ui_control", "assets", "out_of_screen.png");
 
 export default async function prnt_scrn(): Promise<string>{
     try {
@@ -7,8 +10,8 @@ export default async function prnt_scrn(): Promise<string>{
         const screenRegion = new Region(mousePosition.x - 100, mousePosition.y - 100, 200, 200);
         await screen.captureRegion("image.png", screenRegion);
         return readFileSync("image.png", {encoding: "base64"})
-    }
+   }
     catch (err) {
-        return readFileSync("image.png", {encoding: "base64"})
+        return readFileSync(errorPath, {encoding: "base64"})
     }
 }
