c3c32eabd7fe9165f32e54c69d4c01f6df80aecc
Dark_Soul <irina2002.nikolaeva@yandex.ru>
2023-01-23 23:52:36 +0300

    feat: implement screen printing without saving

--- src/ui_control/printScreen.ts
+++ src/ui_control/printScreen.ts
@@ -1,15 +1,18 @@
 import {screen, Region, mouse,} from "@nut-tree/nut-js"
 import {readFileSync} from "fs"
 import { resolve } from "path";
+import Jimp from "jimp"
 
 const errorPath = resolve("src", "ui_control", "assets", "out_of_screen.png");
 
-export default async function prnt_scrn(): Promise<string>{
-    try {
+export default async function prnt_scrn(): Promise<string>{ 
+   try {
         const mousePosition = await mouse.getPosition();
         const screenRegion = new Region(mousePosition.x - 100, mousePosition.y - 100, 200, 200);
-        await screen.captureRegion("image.png", screenRegion);
-        return readFileSync("image.png", {encoding: "base64"})
+        const imageBuffer = await (await screen.grabRegion(screenRegion)).toRGB();
+        const jimpImg = new Jimp({data: imageBuffer.data, width: imageBuffer.width, height: imageBuffer.height}, ()=>{});
+        const jimpBuf = await jimpImg.getBufferAsync(Jimp.MIME_PNG);
+        return jimpBuf.toString("base64");
    }
     catch (err) {
         return readFileSync(errorPath, {encoding: "base64"})
